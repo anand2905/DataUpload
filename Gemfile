20260e8704461b671b48b5248bd6701f681603e3
Kumar Anand <anand29.ku@gmail.com>
2022-01-16 22:29:12 +0530

    Picture Upload Crud

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'simple_form', '~> 5.1'
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+gem 'devise', '~> 4.2'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,49 @@
+class PicturesController < ApplicationController
+	before_action :find_picture, only: [:show, :edit, :update, :destroy]
+	def index
+		@pictures = Picture.all.order("created_at DESC")
+	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def show
+	end
+
+	def create
+		@picture = Picture.new(picture_params)
+
+		if @picture.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @picture.update(picture_params)
+			redirect_to picture_path(@picture)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@picture.destroy
+		redirect_to root_path
+	end
+	
+	private
+
+		def picture_params
+			params.require(:picture).permit(:title, :description)
+		end
+
+		def find_picture
+			@picture = Picture.find(params[:id])
+		end
+end
